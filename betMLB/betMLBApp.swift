d0d6d60366512abfe34611606fb1aee2d0fecc0d
Andrew Xin <andrewx@bu.edu>
2024-08-02 19:05:06 -0700

    Initial Commit

---
+++ betMLB/betMLBApp.swift
@@ -0,0 +1,17 @@
+//
+//  betMLBApp.swift
+//  betMLB
+//
+//  Created by Andrew Xin on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct betMLBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
